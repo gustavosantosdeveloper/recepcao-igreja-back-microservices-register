692595978e4956685de4a340a97fb5db8c01165c
Gustavo Santos <gustavocarmo.prestserv@petrobras.com.br>
2024-08-23 20:12:30 -0300

    primeiro commit

---
+++ src/test/java/br/com/igrejas/RecepcaoRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igrejas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepcaoRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
